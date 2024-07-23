41b8aba6788e188b8d851ff03b4e25c572d14c89
miedgo <miedgo@upvnet.upv.es>
2024-07-23 18:08:52 +0200

    Materials inherit from named tuple

--- Classes/TradeOffer.py
+++ Classes/TradeOffer.py
@@ -5,7 +5,7 @@ class TradeOffer:
     """
     Clase que representa los intercambios entre jugadores.
     """
-    def __init__(self, gives=Materials(), receives=Materials()):
+    def __init__(self, gives=Materials(0,0,0,0,0), receives=Materials(0,0,0,0,0)):
         self.gives = gives
         self.receives = receives
         return
