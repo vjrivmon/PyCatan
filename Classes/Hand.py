cdefa5af036a46bdeda3b4367057d816f0b3bf56
miedgo <miego.cvcv@gmail.com>
2024-08-28 23:06:05 +0200

    Improve Hand and solve bug

--- Classes/Hand.py
+++ Classes/Hand.py
@@ -1,4 +1,6 @@
 from Classes.Materials import Materials
+from functools import singledispatchmethod
+
 
 
 class Hand:
@@ -8,47 +10,52 @@ class Hand:
 
     def __init__(self):
         self.resources = Materials(0,0,0,0,0)
-        return
 
-    def add_material(self, resource, amount):
+
+    def add_material(self, resource_id, amount):
         """
-        Suma amount al material seleccionado (si es negativo lo resta de la cantidad actual).
-        Si se le pasa una lista se convierte en una función recusiva de sí misma.
-        :param resource: (int o list) tipo de recurso a añadir.
+        Suma amount al material o materiales seleccionados (si es negativo lo resta).
+        :param resource: (int o list[int]) tipo de recurso(s) a añadir.
         :param amount: (int) cantidad del recurso a añadir.
         :return: None
         """
-        if isinstance(resource, list):
-            for material in resource:
-                self.add_material(material, amount)
+        # si es un entero lo convertimos en una lista
+        if isinstance(resource_id, int):
+            resource_id = [resource_id]
 
-        else:
-            self.resources = self.resources.add_from_id(resource, amount)
+        # logica antigua
+        # for id in resource_id:
+        #     if amount + self.resources.get_from_id(id) < 0:
+        #         return
+        #     self.resources = self.resources.add_from_id(id, amount)
+        # return
 
-        return
+        # lo convertimos a materiales para poder sumarlo: ([1,3],5) -> [0,5,0,5,0]
+        resources = Materials(*[ amount if id in resource_id else 0 for id in range(5)]) 
+        tmp_resources = self.resources + resources
+
+        # comprobamos que ninguno sea negativo, un jugador no puede tener materiales negativos
+        if any([n < 0 for n in tmp_resources]):
+            return
+
+        self.resources = tmp_resources
+
 
     def remove_material(self, resource, amount):
         """
-        Resta amount al material seleccionado (si es negativo lo suma de la cantidad actual).
-        Si se le pasa una lista se convierte en una función recusiva de sí misma.
-        :param resource: (int o list) tipo de recurso a quitar.
+        Resta amount al material o materiales seleccionados (si es negativo lo suma).
+        :param resource: (int o list[int]) tipo de recurso(s) a quitar.
         :param amount: (int)cantidad del recurso a quitar.
         :return: None
         """
-        if isinstance(resource, list):
-            for material in resource:
-                self.remove_material(material, amount)
-        else:
-            if self.get_from_id(resource) >= amount:
-                self.add_material(resource, (amount * -1))
-        return
+        self.add_material(resource, -amount)
+
 
     def get_from_id(self, material_id):
         return self.resources.get_from_id(material_id)
 
     def get_total(self):
-        return (self.resources.cereal + self.resources.mineral + self.resources.clay
-                + self.resources.wood + self.resources.wool)
+        return sum(self.resources)
 
     def __str__(self):
-        return 'Hand(' + str(self.resources) + ')'
+        return f'Hand: {str(self.resources)}' 
